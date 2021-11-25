f123f5d0168fc7dc099e0becc5b7b29f3b574725
micheaol <micheaol80@gmail.com>
2021-11-25 02:57:24 +0100

    use of linter disable has been reduced

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
-
 import './Button.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
