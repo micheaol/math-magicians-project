19ffa8abd5b3cbe1be7162a4e015060d5511b89d
micheaol <micheaol80@gmail.com>
2021-11-30 17:40:23 +0100

    enable linters

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,25 @@
-/* eslint-disable max-len */
 import React from 'react';
 
 const Home = () => (
   <div className="home-wrapper">
     <h1>Welcome to our page!</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores perspiciatis sequi ad provident sit. Ut mollitia minus facere ipsum rem labore, magni repellendus aut atque eveniet, laborum quaerat delectus enim!</p>
+    <div>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+    </div>
 
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores perspiciatis sequi ad provident sit. Ut mollitia minus facere ipsum rem labore, magni repellendus aut atque eveniet, laborum quaerat delectus enim!</p>
+    <div>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+    </div>
 
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores perspiciatis sequi ad provident sit. Ut mollitia minus facere ipsum rem labore, magni repellendus aut atque eveniet, laborum quaerat delectus enim!</p>
+    <div>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+      <p>Lo,ipsumdolorsit amet consecteturadipisicing elit Asperiores adipisicingelit Asperiores</p>
+    </div>
   </div>
 );
 
